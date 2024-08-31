03ee74333084056983df4153adea4b9d83db02e5
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-01 01:16:51 +0200

    Added cors

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require("./routes/config");
 
 const port = process.env.APP_PORT;
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}`);
   });
--- routes/config.js
+++ routes/config.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const { checkConnection } = require('../database/server');
 
 const router = require("./router");
 
 checkConnection();
+
+app.use(cors());
 
 app.use("/api", router);
 
