d61d7ed990d8e5b7c9b2d7d5ca38d4f8c9078f26
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-10 23:43:49 +0200

    Added path for login 🛂

--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const userActions = require("../controllers/UserActions");
 const testActions = require("../controllers/TestActions");
-
+const { login } = require("../controllers/loginActions");
+const { comparePassword } = require("../midlleware/compare");
 
 //  ******* PATH ******* \\
 
+router.get("/", testActions.test);
 
-router.get('/', testActions.test);
+router.post("/login", comparePassword, login);
 
-router.get('/users', userActions.browse);
-router.get('/users/:email', userActions.read);
-router.post('/users', userActions.add);
+router.get("/users", userActions.browse);
+router.get("/users/:email", userActions.read);
+router.post("/users", userActions.add);
 
 module.exports = router;
