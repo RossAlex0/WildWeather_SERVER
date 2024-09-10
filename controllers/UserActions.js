d28aa0e4e002801a5dc5261217e3ac3c875f0f2d
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-10 23:31:39 +0200

    Prettier formatage🗒️

--- controllers/UserActions.js
+++ controllers/UserActions.js
@@ -1,40 +1,43 @@
 const user = require("../database/models/userSchema");
 
-
-const browse = async(req, res, next) => {
-    try {
-      const users = await user.find(); 
-      res.json(users); 
-    } catch (err) {
-      next(err)
-    }
+const browse = async (req, res, next) => {
+  try {
+    const users = await user.find();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const read = async (req, res, next) => {
   try {
-      const { email } = req.params;
-      const userFound = await user.findOne({ mail: email });
+    const { email } = req.params;
+    const userFound = await user.findOne({ mail: email });
 
-      if (!userFound) {
-        res.status(404).json({ message: 'User not found' });
-      } else {
-        res.status(200).json(userFound);
-      }
+    if (!userFound) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.status(200).json(userFound);
+    }
   } catch (err) {
-      next(err)
+    next(err);
   }
 };
 
-  const add = async (req, res) => {
-    try {
-        const { name, mail, city, password } = req.body;
-        const newUser = new user({ name, mail, city, password });
-        await newUser.save();
-        console.log('New User:', req.body);
-        res.status(201).json({ message: 'New user created' });
-    } catch (err) {
-        next(err)
-    }
+// *EDIT* //
+
+const add = async (req, res, next) => {
+  try {
+    const { name, mail, city, password } = req.body;
+    const newUser = new user({ name, mail, city, password });
+    await newUser.save();
+    console.log("New User:", req.body);
+    res.status(201).json({ message: "New user created" });
+  } catch (err) {
+    next(err);
+  }
 };
 
-  module.exports = { browse, add, read };
+// *DELETE* //
+
+module.exports = { browse, add, read };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
-const app = require("./routes/config");
+const app = require("./config");
 
 const port = process.env.APP_PORT;
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
-  });
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
+});
