9097f036e9b650af0e207ce46989a4bd38ed6867
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-13 23:11:16 +0200

    Created edit function into userActions ⬆

--- controllers/UserActions.js
+++ controllers/UserActions.js
@@ -1,8 +1,8 @@
-const user = require("../database/models/userSchema");
+const User = require("../database/models/userSchema");
 
 const browse = async (req, res, next) => {
   try {
-    const users = await user.find();
+    const users = await User.find();
     res.json(users);
   } catch (err) {
     next(err);
@@ -12,7 +12,7 @@ const browse = async (req, res, next) => {
 const read = async (req, res, next) => {
   try {
     const { email } = req.params;
-    const userFound = await user.findOne({ mail: email });
+    const userFound = await User.findOne({ mail: email });
 
     if (!userFound) {
       res.status(404).json({ message: "User not found" });
@@ -24,12 +24,30 @@ const read = async (req, res, next) => {
   }
 };
 
-// *EDIT* //
+const edit = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, city, hashedPassword } = req.body;
+
+    const userFound = await User.findById(id);
+    if (!userFound) {
+      res.status(404).json({ message: "User not found" });
+    }
+
+    await userFound.update(name, city, hashedPassword);
+
+    res
+      .status(200)
+      .json({ message: "Utilisateur mis à jour avec succès", userFound });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const add = async (req, res, next) => {
   try {
     const { name, mail, city, password } = req.body;
-    const newUser = new user({ name, mail, city, password });
+    const newUser = new User({ name, mail, city, password });
     await newUser.save();
     console.log("New User:", req.body);
     res.status(201).json({ message: "New user created" });
@@ -38,6 +56,4 @@ const add = async (req, res, next) => {
   }
 };
 
-// *DELETE* //
-
-module.exports = { browse, add, read };
+module.exports = { browse, read, edit, add };
