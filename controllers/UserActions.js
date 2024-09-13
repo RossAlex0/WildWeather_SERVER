c2d1b16eceb10431885f610705fd9ddd80653dde
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-13 23:37:18 +0200

    Created destroy function into userActions ❌

--- controllers/UserActions.js
+++ controllers/UserActions.js
@@ -3,7 +3,8 @@ const User = require("../database/models/userSchema");
 const browse = async (req, res, next) => {
   try {
     const users = await User.find();
-    res.json(users);
+
+    res.status(200).send(users);
   } catch (err) {
     next(err);
   }
@@ -13,12 +14,11 @@ const read = async (req, res, next) => {
   try {
     const { email } = req.params;
     const userFound = await User.findOne({ mail: email });
-
     if (!userFound) {
-      res.status(404).json({ message: "User not found" });
-    } else {
-      res.status(200).json(userFound);
+      res.status(404).send("User not found");
     }
+
+    res.status(200).send(userFound);
   } catch (err) {
     next(err);
   }
@@ -28,17 +28,13 @@ const edit = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { name, city, hashedPassword } = req.body;
-
     const userFound = await User.findById(id);
     if (!userFound) {
-      res.status(404).json({ message: "User not found" });
+      res.status(404).send("User not found");
     }
-
     await userFound.update(name, city, hashedPassword);
 
-    res
-      .status(200)
-      .json({ message: "Utilisateur mis à jour avec succès", userFound });
+    res.status(200).send("Utilisateur mis à jour avec succès : ", userFound);
   } catch (err) {
     next(err);
   }
@@ -49,11 +45,25 @@ const add = async (req, res, next) => {
     const { name, mail, city, password } = req.body;
     const newUser = new User({ name, mail, city, password });
     await newUser.save();
-    console.log("New User:", req.body);
-    res.status(201).json({ message: "New user created" });
+
+    res.status(201).send("New user created : ", newUser);
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { browse, read, edit, add };
+const destroy = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      res.status(404).send("User not found");
+    }
+
+    res.status(200).send("User deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { browse, read, edit, add, destroy };
