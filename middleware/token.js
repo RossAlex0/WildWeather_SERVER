3bac3412b30210f34a22f5b2fd631f457e385678
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-15 00:37:23 +0200

    Created middleware for create token 🚀

--- middleware/token.js
+++ middleware/token.js
@@ -1,1 +1,19 @@
 const jwt = require("jsonwebtoken");
+
+const createToken = async (req, res, next) => {
+  try {
+    const payload = req.user;
+
+    const token = await jwt.sign(payload, process.env.APP_SECRET, {
+      expiresIn: "1y",
+    });
+
+    req.token = token;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createToken };
