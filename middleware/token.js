5392c729d26b805e8a8deb968a91bfdb78dc4da3
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-17 22:29:26 +0200

    Token Management 🗄️

--- middleware/token.js
+++ middleware/token.js
@@ -18,19 +18,15 @@ const createToken = async (req, res, next) => {
 
 const verifyToken = async (req, res, next) => {
   try {
-    const { authTokenWildApp } = req.cookies;
+    const { token } = req.body;
 
-    await jwt.verify(
-      authTokenWildApp,
-      process.env.APP_SECRET,
-      (err, decoded) => {
-        if (err) {
-          return res.status(401).send("Invalid token");
-        }
-        req.decoded = decoded;
-        next();
+    await jwt.verify(token, process.env.APP_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send("Invalid token");
       }
-    );
+      req.decoded = decoded;
+      next();
+    });
   } catch (error) {
     next(error);
   }
